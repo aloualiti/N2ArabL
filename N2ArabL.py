4be3722cbb89da75da82c483551f015c063d7f62
Mounir AL OUALITI <aloualiti@gmail.com>
2018-06-01 12:52:45 +0000

    Make code more comprehensive

--- N2ArabL.py
+++ N2ArabL.py
@@ -15,7 +15,8 @@ def nTol(num):
     numdict[10] = 'عشرة'
     numdict[11] = 'احدا عشر'
     numdict[12] = 'اثنا عشر'
-  
+
+    
     numdict[20] = 'عشرون'
     numdict[30] = 'ثلاثون'
     numdict[40] = 'أربعون'
@@ -24,7 +25,6 @@ def nTol(num):
     numdict[70] = 'سبعون'
     numdict[80] = 'ثمانون'
     numdict[90] = 'تسعون'
- 
     numdict[100] = 'مائة'
     numdict[200] = 'مائتان'
     numdict[300] = 'ثلاثمائة'
@@ -34,42 +34,53 @@ def nTol(num):
     numdict[700] = 'سبعمائة'
     numdict[800] = 'ثمانمائة'
     numdict[900] = 'تسعمائة'
-
+    
     numdict[1000] = ' ألف '
     numdict[2000] = ' ألفان '
     millier= " الاف "
 
     numdict[1000000] = ' مليون '
     numdict[2000000] = ' مليونان '
+        
     million= " ملايين "
-    
+
     result=numdict.get(num)
     if result==None:
         numstr=str(num)
         lenstr=len(numstr)
         if lenstr==2:
-            if int(numstr[0])==1:
-                return numdict[int(numstr[1])] + " " + numdict[int(numstr[0])*10]
-            return numdict[int(numstr[1])] + " و " + numdict[int(numstr[0])*10]
+            #0 to 99
+            units=int(numstr[1])
+            dozens=int(numstr[0])
+            if dozens==1:
+                return numdict[units] + " " + numdict[dozens*10]
+            return numdict[units] + " و " + numdict[dozens*10]
         elif lenstr==3:
-            return numdict[int(numstr[0]) * 100] + " و " + nTol(int(numstr[1:]))
+            # 0 to 999
+            hundreds=int(numstr[0])
+            restdozens=int(numstr[1:])
+            return numdict[hundreds * 100] + " و " + nTol(restdozens)
         elif 7>lenstr>=4:
-            if int(numstr[:-3])<=10:
-                return numdict.get(int(numstr[:-3])*1000, nTol(int(numstr[:-3])) + millier) + " و "  + nTol(int(numstr[-3:]))
+            # 0 to 999 999
+            thousands=int(numstr[:-3])
+            resthundreds=int(numstr[-3:])
+            if thousands<=10:
+                return numdict.get(thousands*1000, nTol(thousands) + millier) + " و "  + nTol(resthundreds)
             else:
-                return nTol(int(numstr[:-3])) + numdict[1000] + "و" + nTol(int(numstr[-3:]))
+                return nTol(thousands) + numdict[1000] + "و" + nTol(resthundreds)
         elif lenstr>=7:
-            if int(numstr[:-6])<=10:
-                return numdict.get(int(numstr[:-6])*1000000, nTol(int(numstr[:-6])) + million) + " و "  + nTol(int(numstr[-6:]))
+            # 0 to 999 999 999
+            millions=int(numstr[:-6])
+            restthousands=int(numstr[-6:])
+            if millions<=10:
+                return numdict.get(millions*1000000, nTol(millions) + million) + " و "  + nTol(restthousands)
             else:
-                return nTol(int(numstr[:-6])) + numdict[1000000] + "و" + nTol(int(numstr[-6:]))
+                return nTol(millions) + numdict[1000000] + "و" + nTol(restthousands)
             
-
-                  
     return result
 x=0
-while x<1000000000:
+while x<1000000000000:
     x= input (" : المرجو منكم ادخال رقم\n")
     print nTol(x)
+
     
-print "اعتذر لم أتعلم بعد كتابة الأرقام الاكبر من 99999999"    
