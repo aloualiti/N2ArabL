c99fe5b16f00e29d6842dc6a762557a11876122b
MOUNIR AL OUALITI <ALOUALITI@GMAIL.COM>
2018-06-01 00:06:48 +0000

    numbers less than 99

--- N2ArabL.py
+++ N2ArabL.py
@@ -12,14 +12,33 @@ def nTol(num):
     numdict[8] = 'ثمانية'
     numdict[9] = 'تسعة'
 
+    numdict[10] = 'عشرة'
+    numdict[11] = 'احدا عشر'
+    numdict[12] = 'اثنا عشر'
+  
+    numdict[20] = 'عشرون'
+    numdict[30] = 'ثلاثون'
+    numdict[40] = 'أربعون'
+    numdict[50] = 'خمسون'
+    numdict[60] = 'ستون'
+    numdict[70] = 'سبعون'
+    numdict[80] = 'ثمانون'
+    numdict[90] = 'تسعون'
  
 
-    result=numdict.get(num, "اعتذر لم أتعلم بعد كتابة الأرقام الاكبر من 9")
-           
+    result=numdict.get(num)
+    if result==None:
+        numstr=str(num)
+        lenstr=len(numstr)
+        if lenstr==2:
+            if int(numstr[0])==1:
+                return numdict[int(numstr[1])] + " " + numdict[int(numstr[0])*10]
+            return numdict[int(numstr[1])] + " و " + numdict[int(numstr[0])*10]
+                  
     return result
 x=0
-while x<10:
+while x<100:
     x= input (" : المرجو منكم ادخال رقم\n")
     print nTol(x)
-
     
+print "9اعتذر لم أتعلم بعد كتابة الأرقام الاكبر من 9"    
